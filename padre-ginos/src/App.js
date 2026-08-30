39428886fd0e406b42609c40b9052db4b1d59745
Vimukthi Perera <vimper007@gmail.com>
2026-08-30 12:32:21 +0530

    feat: initialize React project with Vite configuration and basic Pizza component structure

--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const Pizza = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
